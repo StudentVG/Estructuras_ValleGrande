5df97a504bd62d6ae595714cd2a8b2e1b5c2ca67
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2026-04-22 09:47:43 -0500

    fix: eliminar valores predeterminados en la configuración de Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GithubAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-     apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAV45CzJr-y-veQFkqwv4B_ndH4esv2MCk",
-     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "estructurasvg.firebaseapp.com",
-     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "estructurasvg",
-     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "estructurasvg.firebasestorage.app",
-     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "758008982885",
-     appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:758008982885:web:907283539ec82f682a058c",
+     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+     appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
